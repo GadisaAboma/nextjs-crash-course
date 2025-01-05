1a7d863fce1d814295bfeb46ed39d1ea15682bf3
Gadisa Aboma <gadaboma@gmail.com>
2025-01-05 21:43:05 +0300

    implemented loading and error handler

---
+++ app/(root)/about/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Page = () => {
+  throw Error("Not implemented");
+  return <div>About route</div>;
+};
+
+export default Page;
---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Loading = () => {
+  return <div>__________________</div>;
+};
+
+export default Loading;
